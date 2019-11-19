5a858edaf37d26c0f6b5801c20a4da7b727d4f1f
peerre <pedrovrodrigues32@gmail.com>
2019-11-19 17:18:44 +0000

    Random Forest

--- Lab5_Mining.r
+++ Lab5_Mining.r
@@ -1,4 +1,6 @@
 customersFull = read.csv("customer.tsv", header = T, sep = "\t")
+customersFull$MONEY_MONTLY_OVERDRAWN <- as.numeric(customersFull$MONEY_MONTLY_OVERDRAWN)
+customersFull$LTV <- as.numeric(customersFull$LTV)
 customers = customersFull
 customers[,c("CUSTOMER_ID", "LAST", "FIRST", "STATE", "HAS_CHILDREN", 
              "N_TRANS_ATM", "N_TRANS_TELLER", "N_TRANS_KIOSK", 
@@ -23,10 +25,28 @@ customers.train <- customers.train[aux,]
 customers.train.target <- customers.train.target[aux,]
 
 customers.train[,c("LTV", "LTV_BIN")] <- NULL
-fit <- rpart(BUY_INSURANCE~., data = customers.train)
+fit <- rpart(BUY_INSURANCE~., data = customers.train, method = "anova")
 rpart.plot(fit)
 customers.test[,c("LTV", "LTV_BIN")] <- NULL
 predfit <- predict(fit, customers.test, type = "class")
 confusao = table(predfit, customers.test.target$BUY_INSURANCE)
 acuracia = sum(diag(confusao))/sum(confusao)
+p0 = nrow(customersFull[customersFull$BUY_INSURANCE == "No",])/nrow(customersFull)
+kappa = (acuracia-p0)/(1-p0)
+precisao = diag(confusao)[1]/sum(confusao[1,])
+
+# Teste com Random Forest
+library(randomForest)
+library(caTools)
+
+#customers.train[,c("MONEY_MONTLY_OVERDRAWN","PROFESSION", "LTV", "LTV_BIN")] <- NULL
+customers.train[,c("PROFESSION")] <- NULL
+
+summary(customers.train)
+rf <- randomForest(BUY_INSURANCE~., data = customers.train)
+predfit <- predict(rf, customers.test, type = "class")
+confusao = table(predfit, customers.test.target$BUY_INSURANCE)
+acuracia = sum(diag(confusao))/sum(confusao)
+p0 = nrow(customersFull[customersFull$BUY_INSURANCE == "No",])/nrow(customersFull)
+kappa = (acuracia-p0)/(1-p0)
 precisao = diag(confusao)[1]/sum(confusao[1,])
